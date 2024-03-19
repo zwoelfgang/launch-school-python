3c669524eeebe12d90abe5f45b963f22e5bcd377
Wolf Timm <wolfktimm@gmail.com>
2024-03-19 11:26:11 -0700

    Further abstracted game logic.

--- py101/lesson2/rock_paper_scissors.py
+++ py101/lesson2/rock_paper_scissors.py
@@ -1,5 +1,6 @@
 import random
 import os
+import platform
 
 VALID_CHOICES = {
     ('(r)ock', 'r', 'rock'): 'rock',
@@ -9,23 +10,39 @@ VALID_CHOICES = {
     ('(l)izard', 'l', 'lizard'): 'lizard'
 }
 
+WINNING_MOVES = {
+    'rock': ('scissors', 'lizard'),
+    'paper': ('rock', 'spock'),
+    'scissors': ('paper', 'lizard'),
+    'spock': ('rock', 'scissors'),
+    'lizard': ('paper', 'spock')
+}
+
+TOTAL_GAMES = 5
+
 def prompt(message):
     print(r'\\', message, r'//')
 
 def unpack_valid_choices():
     return [item for item_list in list(VALID_CHOICES) for item in item_list]
 
-def unpack_displayable_choices():
-    return [item for item in choices_list
-            if choices_list.index(item) % len(list(VALID_CHOICES)[0]) == 0]
+def unpack_displayable_choices(choices):
+    return [item for item in choices
+            if choices.index(item) % len(list(VALID_CHOICES)[0]) == 0]
 
-def get_user_choice(choices):
-    prompt(f"Choose one: {', '.join(choices)}.")
+def unpack():
+    choices = unpack_valid_choices()
+    displayable_choices = unpack_displayable_choices(choices)
+
+    return choices, displayable_choices
+
+def get_player_choice(choices_list):
+    prompt(f"Choose one: {', '.join(choices_list)}.")
     choice = input('>> ').lower()
 
     return choice
 
-def map_user_choice(string):
+def map_player_choice(string):
     idx = 0
     choice_map = {}
     choice_key = None
@@ -40,14 +57,14 @@ def map_user_choice(string):
 
     return choice_map, choice_key
 
-def check_user_choice(choice, display_list, total_list):
-    choice_map, key = map_user_choice(choice)
+def check_player_choice(choice, display_list, total_list):
+    choice_map, key = map_player_choice(choice)
 
     while choice not in total_list:
         prompt(f"That choice is invalid. \
 Please enter {', '.join(display_list)}")
         choice = input('>> ').lower()
-        choice_map, key = map_user_choice(choice)
+        choice_map, key = map_player_choice(choice)
 
     return choice_map, key
 
@@ -57,8 +74,8 @@ def get_computer_choice():
 def display_choices(player, computer):
     prompt(f'You chose {player}, computer chose {computer}.')
 
-def play_again(player_num):
-    if player_num == 5:
+def play_again(player_score):
+    if player_score == 5:
         prompt('Congratulations! You won best out of 5. \
 Would you like to play again? (Y)es or (n)o.')
     else:
@@ -78,31 +95,12 @@ def check_answer(string):
     return string
 
 def determine_winner(player, computer):
-    match (player, computer):
-        case ('rock', 'scissors') | \
-             ('rock', 'lizard') | \
-             ('paper', 'rock') | \
-             ('paper', 'spock') | \
-             ('scissors', 'paper') | \
-             ('scissors', 'lizard') | \
-             ('spock', 'rock') | \
-             ('spock', 'scissors') | \
-             ('lizard', 'paper') | \
-             ('lizard', 'spock'):
-            return 'player'
-        case ('scissors', 'rock') | \
-             ('lizard', 'rock') | \
-             ('rock', 'paper') | \
-             ('spock', 'paper') | \
-             ('paper', 'scissors') | \
-             ('lizard', 'scissors') | \
-             ('rock', 'spock') | \
-             ('scissors', 'spock') | \
-             ('paper', 'lizard') | \
-             ('spock', 'lizard'):
-            return 'computer'
-        case _:
-            return None
+    if computer in WINNING_MOVES[player]:
+        return 'player'
+    elif player in WINNING_MOVES[computer]:
+        return 'computer'
+    else:
+        return None
 
 def display_winner(won, player, computer):
     if won in ['player', 'computer']:
@@ -123,52 +121,62 @@ def increment_score(won, player, computer):
 def display_score(player, computer):
     prompt(f"Your score is {player}. The computer's score is {computer}.")
 
-while True:
-    player_score = 0
-    computer_score = 0
-    num_games = 0
-    winner = ''
-    user_choice_map = {}
-    user_key = ()
-    computer_choice = ''
-
-    while player_score < 5 and computer_score < 5:
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
         os.system('clear')
 
+def game_loop(player_score, computer_score, num_games):
+    while player_score < TOTAL_GAMES and computer_score < TOTAL_GAMES:
+        clear_screen()
+
         if num_games > 0:
-            display_winner(winner, user_choice_map[user_key], computer_choice)
+            display_winner(winner, map_choice[key_choice], choice_comp)
 
         display_score(player_score, computer_score)
-
-        choices_list = unpack_valid_choices()
-        displayable_choices_list = unpack_displayable_choices()
-
-        user_choice = get_user_choice(displayable_choices_list)
-        user_choice_map, user_key = check_user_choice(user_choice,
-                                                      displayable_choices_list,
-                                                      choices_list)
-
-        computer_choice = get_computer_choice()
-
-        display_choices(user_choice_map[user_key], computer_choice)
-        winner = determine_winner(user_choice_map[user_key], computer_choice)
-
+        choices, displayable_choices = unpack()
+        choice = get_player_choice(displayable_choices)
+        map_choice, key_choice = check_player_choice(choice,
+                                                     displayable_choices,
+                                                     choices)
+        choice_comp = get_computer_choice()
+        display_choices(map_choice[key_choice], choice_comp)
+        winner = determine_winner(map_choice[key_choice], choice_comp)
         player_score, computer_score = increment_score(winner,
                                                        player_score,
                                                        computer_score)
 
         num_games += 1
+    
+    return winner, \
+           map_choice[key_choice], \
+           choice_comp, \
+           player_score, \
+           computer_score
 
-    os.system('clear')
-
-    display_winner(winner, user_choice_map[user_key], computer_choice)
-    display_score(player_score, computer_score)
-
-    answer = play_again(player_score)
-
-    if answer[0] == 'y':
+def play_game():
+    while True:
         player_score = 0
         computer_score = 0
-    elif answer[0] == 'n':
-        os.system('clear')
-        break
+        num_games = 0
+
+        winner, \
+        player, \
+        computer, \
+        player_score, \
+        computer_score = game_loop(player_score, computer_score, num_games)
+
+        clear_screen()
+        display_winner(winner, player, computer)
+        display_score(player_score, computer_score)
+        answer = play_again(player_score)
+
+        if answer[0] == 'y':
+            player_score = 0
+            computer_score = 0
+        elif answer[0] == 'n':
+            clear_screen()
+            break
+
+play_game()
